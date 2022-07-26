e79d6bb6981037a0681753d32593732d634178d2
yunji <yunji@yunji-mac.local>
2022-07-26 23:30:23 +0900

    add 알고리즘 푸는 중

---
+++ study/Programmers42626.java
@@ -0,0 +1,23 @@
+package study;
+
+import java.util.TreeSet;
+
+public class Programmers42626 {
+
+    public static void main(String[] args) {
+
+        int[] value = {1,2,3,9,10,12};
+
+        System.out.println(solution(value,7));
+
+    }
+
+    private static int solution(int[] scoville, int K) {
+        int tmp=0;
+        int count=0;
+        for(int i=0 ; i<scoville.length; i++){
+            tmp = scoville[i]+ (scoville[i+1]*2);
+            if(tmp )
+        }
+    }
+}
