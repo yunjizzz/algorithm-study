e727deff2c44861544d24d6df55826d06046beaf
yunji <yunji@yunji-mac.local>
2022-08-12 00:43:51 +0900

    add 12941 진행중

---
+++ study/Programmers12941.java
@@ -0,0 +1,44 @@
+package study;
+
+/*
+ */
+public class Programmers12941 {
+
+	public static void main(String[] args) {
+
+		int[] a = {1, 4, 2};
+		int[] b = {5, 4, 4};
+
+		System.out.println(solution(a, b));
+	}
+
+	public static int solution(int[] arrA, int[] arrB) {
+		int sumA = 0;
+		int sumB = 0;
+		int minus = arrA.length - 1;
+
+		arrA = sort(arrA);
+		arrB = sort(arrB);
+
+		for (int i = 0; i < arrA.length; i++) {
+			sumA += arrA[i] * arrB[minus - i];
+			sumB += arrB[i] * arrA[minus - i];
+		}
+		return Math.min(sumA, sumB);
+	}
+
+	//정렬하는거 구현
+	public static int[] sort(int[] a) {
+		for (int i = 0; i < a.length; i++) {
+			int num = a[i];
+			for (int j = i + 1; j < a.length; j++) {
+				if (num > a[j]) {
+					a[i] = a[j];
+					a[j] = num;
+					break;
+				}
+			}
+		}
+		return a;
+	}
+}
